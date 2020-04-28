0e9f67e63550758b8250b166b7ce11a2a99a5a3c
akoen <arw.koen@gmail.com>
2020-04-28 12:37:30 -0700

    Aruco marker detection

--- aruco/main.cpp
+++ aruco/main.cpp
@@ -55,13 +55,137 @@ void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundC
   }
 }
 
-int main(void) {
+int startWebcamMonitoring(const Mat& cameraMatrix, const Mat& distanceCoefficients, float arucoDimensions) {
+  Mat frame;
+
+  vector<int> markerIds;
+  vector<vector<Point2f>> markerCorners, rejectedCandidates;
+  aruco::DetectorParameters parameters;
+
+  Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);
+
+  VideoCapture vid(0);
+
+  if(!vid.isOpened()) {
+    return -1;
+  }
+
+  namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+  vector<Vec3d> rotationVectors, translationVectors;
+
+  while(true) {
+    if(!vid.read(frame))
+      break;
+
+    aruco::detectMarkers(frame, markerDictionary, markerCorners, markerIds);
+    aruco::estimatePoseSingleMarkers(markerCorners, arucoSquareDimension, cameraMatrix, distanceCoefficients, rotationVectors, translationVectors);
+
+    for(int i = 0; i < markerIds.size(); i++) {
+      aruco::drawAxis(frame, cameraMatrix, distanceCoefficients, rotationVectors[i], translationVectors[i], 0.1f);
+    }
+
+    imshow("Webcam", frame);
+    if(waitKey(30) >= 0) break;
+  }
+
+  return 1;
+}
+
+void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distanceCoefficients) {
+  vector<vector<Point2f>> checkerboardImageSpacePoints;
+  getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+
+  vector<vector<Point3f>> worldSpaceCornerPoints(1);
+
+  createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+  worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+  vector<Mat> rVectors, tVectors;
+  distanceCoefficients = Mat::zeros(8, 1, CV_64F);
+
+  calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
+}
+
+bool saveCameraCalibration(string name, Mat cameraMatrix, Mat distanceCoefficients) {
+  ofstream outStream(name);
+  if(outStream) {
+    uint16_t rows = cameraMatrix.rows;
+    uint16_t columns = cameraMatrix.cols;
+
+    outStream << rows << endl;
+    outStream << columns << endl;
+
+    for(int r = 0; r < rows; r++) {
+      for(int c = 0; c < columns; c++) {
+        double value = cameraMatrix.at<double>(r,c);
+        outStream << value << endl;
+      }
+    }
+
+    rows = distanceCoefficients.rows;
+    columns = distanceCoefficients.cols;
+
+    outStream << rows << endl;
+    outStream << columns << endl;
+
+    for(int r = 0; r < rows; r++) {
+      for(int c = 0; c < columns; c++) {
+        double value = distanceCoefficients.at<double>(r,c);
+        outStream << value << endl;
+      }
+    }
+
+    outStream.close();
+    return true;
+  }
+
+  return false;
+}
+
+bool loadCameraCalibration(string name, Mat& cameraMatrix, Mat& distanceCoefficients) {
+  ifstream inStream(name);
+  if(inStream) {
+    uint16_t rows;
+    uint16_t columns;
+
+    inStream >> rows;
+    inStream >> columns;
+
+    cameraMatrix = Mat(Size(columns, rows), CV_64F);
+
+    for(int r = 0; r < rows; r++) {
+      for(int c = 0; c < columns; c++) {
+        double read = 0.0f;
+        inStream >> read;
+        cameraMatrix.at<double>(r,c) = read;
+        cout << cameraMatrix.at<double>(r,c) << "\n";
+      }
+    }
+    // Distance Coefficients
+    inStream >> rows;
+    inStream >> columns;
+
+    distanceCoefficients = Mat::zeros(rows, columns, CV_64F);
+
+    for(int r = 0; r < rows; r++) {
+      for(int c = 0; c < columns; c++) {
+        double read = 0.0f;
+        inStream >> read;
+        distanceCoefficients.at<double>(r,c) = read;
+        cout << distanceCoefficients.at<double>(r,c) << "\n";
+      }
+    }
+    inStream.close();
+    return true;
+  }
+
+  return false;
+}
+
+void cameraCalibrationProcess(Mat& cameraMatrix, Mat& distanceCoefficients){
   Mat frame;
   Mat drawToFrame;
-
-  Mat cameraMatrix = Mat::eye(3,3, CV_64F);
-
-  Mat distanceCoefficients;
 
   vector<Mat> savedImages;
 
@@ -70,7 +194,7 @@ int main(void) {
   VideoCapture vid(0);
 
   if(!vid.isOpened()) {
-    return 0;
+    return;
   }
 
   int framesPerSecond = 20;
@@ -94,7 +218,42 @@ int main(void) {
       imshow("Webcam", frame);
 
     char character = waitKey(1000 / framesPerSecond);
+
+    switch(character) {
+    case ' ':
+      //save image
+      if(found) {
+        Mat temp;
+        frame.copyTo(temp);
+        savedImages.push_back(temp);
+        cout << "Image registered";
+      }
+      break;
+    case 13: // Return
+      // start calibration
+      if(savedImages.size() > 15)
+        {
+          cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension, cameraMatrix, distanceCoefficients);
+          saveCameraCalibration("calibration-data.txt", cameraMatrix, distanceCoefficients);
+          cout << "Camera calibrated" << endl;
+        }
+      break;
+    case 27: // ESC
+      //exit
+      return;
+    }
   }
+}
+
+int main(int argv, char** argc) {
+
+  Mat cameraMatrix = Mat::eye(3,3, CV_64F);
+
+  Mat distanceCoefficients;
+
+  // cameraCalibrationProcess(cameraMatrix, distanceCoefficients);
+  loadCameraCalibration("calibration-data.txt", cameraMatrix, distanceCoefficients);
+  startWebcamMonitoring(cameraMatrix, distanceCoefficients, 0.011f);
 
   return 0;
 }
